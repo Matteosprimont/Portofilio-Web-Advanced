f63b7950200416a5a1e9bfa8fca3d98a1d57a659
Matteosprimont <matteo.sprimont@student.ehb.be>
2024-05-21 16:47:15 +0200

    niewe functionaliteiten toegvoegd voor de scorekeeper pagina

--- JS/Scorekeeper.js
+++ JS/Scorekeeper.js
@@ -12,19 +12,40 @@ function voegPersoonToe() {
     }
 }
 
+function checkWinnaar() {
+    const aantalRondes = parseInt(rondes.value);
+    namen.forEach(speler => {
+        if (speler.punten >= aantalRondes) {
+            alert(`${speler.naam} heeft gewonnen!`);
+        }
+    });
+}
+
 function toonNamen(){
     namenLijst.innerHTML = '';
     namen.forEach((speler, index) => {
         const li = document.createElement('li');
         li.innerHTML = `
             <span class="naam">${speler.naam}</span>
+            <span class="punten">${speler.punten}</span>
             <button class="verwijder">Verwijder</button>
+            <button class="toevoegen">Punten toevoegen</button>
+            <button class="verwijderen">Punten verwijderen</button>
         `;
         li.querySelector('.verwijder').addEventListener('click', () => {
             namen = namen.filter(n => n.naam !== speler.naam);
             toonNamen();
         });
-
+        li.querySelector('.toevoegen').addEventListener('click', () => {
+            speler.punten++;
+            toonNamen();
+            checkWinnaar();
+        });
+        li.querySelector('.verwijderen').addEventListener('click', () => {
+            speler.punten--;
+            toonNamen();
+            checkWinnaar();
+        });
         namenLijst.appendChild(li);
     });
 }
